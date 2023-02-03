0e794e69f3a4fe6de6ee014704a7c33700c24c4c
Evan Rust <having11@users.noreply.github.com>
2023-02-02 23:28:23 -0600

    Make code compile

--- 2023 led controller/src/main.cpp
+++ 2023 led controller/src/main.cpp
@@ -1,45 +1,49 @@
 #include <Arduino.h>
 #include <FastLED.h>
 #include <Wire.h>
-  
-#define PIN 6
+
+#define PIN 2
 #define LEDNUM 150
-struct RGB
-{
-  uint8_t red;
-  uint8_t green;
-  uint8_t blue;
+
+// Forward declaration
+void receiveEvent(int);
+
+/*
+enum class CommandType
+*/
+
+struct RGBColorData {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+    // uint8_t colors[3];
 };
 
-
-typedef struct Command
-{
-  uint8_t command;
-  union
-  {
+union CommandData {
     uint8_t pattern;
-    RGB color;
-  }
+    RGBColorData color;
+};
+
+struct Command {
+    uint8_t command;
+    CommandData commandData;
 };
 
 constexpr uint8_t slaveAddress = 0x01;
 
-void setup()
-{
+void setup() {
     Wire.begin(slaveAddress);     // join i2c bus with address #4
     Wire.onReceive(receiveEvent); // register event
     Serial.begin(9600);           // start serial for output
 }
 
-void loop()
-{
-  delay(100);
+void loop() {
+    delay(100);
 }
 
 // function that executes whenever data is received from master
 // this function is registered as an event, see setup()
-void receiveEvent(int howMany)
-{
+void receiveEvent(int howMany) {
     while(1 < Wire.available()) // loop through all but the last
     {
       char c = Wire.read(); // receive byte as a character
