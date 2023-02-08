a14910487ee5b7142022e74258519d5622ee8e78
nobody5050 <32989720+nobody5050@users.noreply.github.com>
2023-02-07 23:57:11 -0600

    Create workflow for code formatting (#17)
    * Create workflow for code formatting
    
    * add .styleguide file
    
    * Create .clang-format
    
    * License for default style guide
    
    * change to different formatter workflow
    
    * test Google format
    
    * Auto fix
    
    * clang auto fix
    
    * Fix code style issues with clang_format
    
    ---------
    
    Co-authored-by: having11 <gatoninja236@gmail.com>
    Co-authored-by: 5690 <5690Programmers@gmail.com>
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- src/main/cpp/subsystems/ExampleSubsystem.cpp
+++ src/main/cpp/subsystems/ExampleSubsystem.cpp
@@ -5,24 +5,24 @@
 #include "subsystems/ExampleSubsystem.h"
 
 ExampleSubsystem::ExampleSubsystem() {
-  // Implementation of subsystem constructor goes here.
+    // Implementation of subsystem constructor goes here.
 }
 
 frc2::CommandPtr ExampleSubsystem::ExampleMethodCommand() {
-  // Inline construction of command goes here.
-  // Subsystem::RunOnce implicitly requires `this` subsystem.
-  return RunOnce([/* this */] { /* one-time action goes here */ });
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return RunOnce([/* this */] { /* one-time action goes here */ });
 }
 
 bool ExampleSubsystem::ExampleCondition() {
-  // Query some boolean state, such as a digital sensor.
-  return false;
+    // Query some boolean state, such as a digital sensor.
+    return false;
 }
 
 void ExampleSubsystem::Periodic() {
-  // Implementation of subsystem periodic method goes here.
+    // Implementation of subsystem periodic method goes here.
 }
 
 void ExampleSubsystem::SimulationPeriodic() {
-  // Implementation of subsystem simulation periodic method goes here.
+    // Implementation of subsystem simulation periodic method goes here.
 }
