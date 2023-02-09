a843a1828f9e4b61790ceb226546e37ac031dbe9
5690 <5690Programmers@gmail.com>
2023-02-09 17:52:55 -0600

    Initialize networktables.py and Add write method

--- coral/object_detection_app/utilities/networking.py
+++ coral/object_detection_app/utilities/networking.py
@@ -1,1 +1,13 @@
+from networktables import NetworkTables
+from Datasource import drawboundingboxes
+import logging
+import sys
+
 class Networking:
+
+    def __init__(self, teamnum, server):
+        self.teamnum = teamnum
+        self.server = server
+
+
+    def write(self, output: list):
