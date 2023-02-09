30b7420e4546bb0b202440b09073958cca8fcdf8
Lint Action <lint-action@samuelmeuli.com>
2023-02-09 23:54:24 +0000

    Fix code style issues with Autopep8

--- coral/object_detection_app/utilities/networking.py
+++ coral/object_detection_app/utilities/networking.py
@@ -3,11 +3,11 @@ from Datasource import drawboundingboxes
 import logging
 import sys
 
+
 class Networking:
 
     def __init__(self, teamnum, server):
         self.teamnum = teamnum
         self.server = server
-
 
     def write(self, output: list):
