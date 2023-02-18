aeeb8723d0c406a4bd9eaf928ca8033bd7d5edb7
5690 <5690Programmers@gmail.com>
2023-02-18 12:50:02 -0600

    create straightback functionalcommand

--- src/main/cpp/commands/Autos.cpp
+++ src/main/cpp/commands/Autos.cpp
@@ -5,10 +5,25 @@
 #include "commands/Autos.h"
 
 #include <frc2/command/Commands.h>
+#include <frc2/command/FunctionalCommand.h>
 
-#include "commands/ExampleCommand.h"
+using namespace AutoConstants;
 
-frc2::CommandPtr autos::ExampleAuto(ExampleSubsystem* subsystem) {
-    return frc2::cmd::Sequence(subsystem->ExampleMethodCommand(),
-                               ExampleCommand(subsystem).ToPtr());
+// This is taken from the hatchbotinlined wpilib example
+frc2::CommandPtr autos::StraightBack(DriveSubsystem* m_drive) {
+    return frc2::FunctionalCommand(
+        // Reset encoders on command start
+        [m_drive] { m_drive->ResetEncoders(); },
+        // Drive forward while the command is executing
+        [m_drive] { m_drive->ArcadeDrive(AutoConstants::kAutoDriveSpeed, 0); },
+        // Stop driving at the end of the command
+        [m_drive](bool interrupted) { m_drive->ArcadeDrive(0, 0); },
+        // End the command when the robot's driven distance exceeds the
+        // desired value
+        [m_drive] {
+            return m_drive->GetAverageEncoderDistance() >= AutoConstants::kAutoDriveDistanceInches;
+        },
+        // Requires the drive subsystem
+        {m_drive})
+      .ToPtr();
 }
