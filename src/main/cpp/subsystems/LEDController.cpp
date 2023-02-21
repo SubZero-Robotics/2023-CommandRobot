976704f0357dce290c2ec0ea042aae404f3d07c4
Lint Action <lint-action@samuelmeuli.com>
2023-02-21 20:39:03 +0000

    Fix code style issues with clang_format

--- src/main/cpp/subsystems/LEDController.cpp
+++ src/main/cpp/subsystems/LEDController.cpp
@@ -1,46 +1,35 @@
 #include "subsystems/LEDController.h"
 
-LEDController(uint8_t slaveAddress, I2C::Port port = I2C::kOnboard) :
-    _i2c = std::make_unique<I2C>(port, slaveAddress), _slaveAddress(slaveAddress),
-    _lastCommand(LEDController::CommandType::Off),
-    _lastPattern(LEDController::PatternType::None) {
+LEDController(uint8_t slaveAddress, I2C::Port port = I2C::kOnboard)
+    : _i2c = std::make_unique<I2C>(port, slaveAddress),
+      _slaveAddress(slaveAddress),
+      _lastCommand(LEDController::CommandType::Off),
+      _lastPattern(LEDController::PatternType::None) {}
 
-}
-
-bool LEDController::initialize() {
-    return !_i2c->AddressOnly();
-}
+bool LEDController::initialize() { return !_i2c->AddressOnly(); }
 
 bool LEDController::setOn() {
     _lastCommand = LEDController::CommandType::On;
-    uint8_t buf[1] = { (uint8_t)_lastCommand };
+    uint8_t buf[1] = {(uint8_t)_lastCommand};
     return !_i2c->WriteBulk(buf, 1);
 }
 
 bool LEDController::setOff() {
     _lastCommand = LEDController::CommandType::Off;
-    uint8_t buf[1] = { (uint8_t)_lastCommand };
+    uint8_t buf[1] = {(uint8_t)_lastCommand};
     return !_i2c->WriteBulk(buf, 1);
 }
 
-bool LEDController::setPattern(LEDController::PatternType pattern, bool oneShot = false) {
+bool LEDController::setPattern(LEDController::PatternType pattern,
+                               bool oneShot = false) {
     _lastCommand = LEDController::CommandType::Pattern;
-    uint8_t buf[3] = {
-        (uint8_t)_lastCommand,
-        (uint8_t)pattern,
-        oneShot
-    };
+    uint8_t buf[3] = {(uint8_t)_lastCommand, (uint8_t)pattern, oneShot};
     return !_i2c->WriteBulk(buf, 3);
 }
 
 bool LEDController::setColor(uint8_t red, uint8_t green, uint8_t blue) {
     _lastCommand = LEDController::CommandType::ChangeColor;
-    uint8_t buf[4] = {
-        (uint8_t)_lastCommand,
-        red,
-        green,
-        blue
-    };
+    uint8_t buf[4] = {(uint8_t)_lastCommand, red, green, blue};
     return !_i2c->WriteBulk(buf, 4);
 }
 
