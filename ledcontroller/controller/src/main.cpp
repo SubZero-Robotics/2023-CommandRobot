c43477fec9077b59a969c49c1c1fc625c2c2dade
Evan Rust <having11@users.noreply.github.com>
2023-02-22 18:15:48 -0600

    RGB LED Controller (#21)
    * create arduino project
    
    * created led kicad board
    
    * base code for LED
    
    * Make code compile
    
    * defined commands
    
    * I2C buffer processing
    
    * handle i2c receive
    
    * parse I2C data
    
    * Add led controller tests
    
    * Update how colors are handled
    
    * Update PCB
    
    * Move commands to external file
    
    * created commands.h
    
    * Add PatternRunner class
    
    * Update PCB
    
    * Fix code style issues with clang_format
    
    * Update PR template
    
    * Added patterns RGBFade, HackerMode, Chase, Wipe
    
    * Fix code style issues with clang_format
    
    * Finished Implementing patterns
    
    * Fix code style issues with clang_format
    
    * Add ReadPatternDone command
    
    * Fix code style issues with clang_format
    
    * RGB LED strip test menu
    
    * Fix code style issues with clang_format
    
    * Add ground plane to layer 2
    
    * Add Uno support
    
    * Get LEDs to show
    
    * Fix code style issues with clang_format
    
    * Fixed RGBFade, Blink, SetColor, and HackerMode
    
    * Fix code style issues with clang_format
    
    * Fix reading color
    
    * Start new pattern after setting
    
    * Fix code style issues with clang_format
    
    * Add case
    
    * RIO I2C Client (#23)
    
    * RIO client
    
    * Fix code style issues with clang_format
    
    * fix errors
    
    * Fix code style issues with clang_format
    
    * errors
    
    * Fix code style issues with clang_format
    
    ---------
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>
    
    ---------
    
    Co-authored-by: 5690 <5690Programmers@gmail.com>
    Co-authored-by: TheOnlyNameRemaining <jameswcornell@gmail.com>
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>
    Co-authored-by: WowMuchDoge <nolthwaits@esko.k12.mn.us>

---
+++ ledcontroller/controller/src/main.cpp
@@ -0,0 +1,256 @@
+#include <Arduino.h>
+#include <FastLED.h>
+#include <Wire.h>
+
+#include "Commands.h"
+#include "Patterns.h"
+
+// Forward declarations
+void receiveEvent(int);
+void requestEvent(void);
+void parseCommand(uint8_t *, size_t);
+// The function signature comes from ExecutePatternCallback in Patterns.h
+// Forward declare new patterns here
+bool executePatternNone(CRGB *, CRGB, uint16_t, uint16_t);
+bool executePatternSetAll(CRGB *, CRGB, uint16_t, uint16_t);
+bool executePatternBlink(CRGB *, CRGB, uint16_t, uint16_t);
+bool executePatternRGBFade(CRGB *, CRGB, uint16_t, uint16_t);
+bool executePatternHackerMode(CRGB *, CRGB, uint16_t, uint16_t);
+bool executePatternChase(CRGB *, CRGB, uint16_t, uint16_t);
+bool executePatternWipe(CRGB *, CRGB, uint16_t, uint16_t);
+
+constexpr uint8_t receiveBufSize = 8;
+// Make sure this is unique on the bus
+constexpr uint8_t slaveAddress = 0x01;
+constexpr uint8_t ledDataOutPin = 2;
+constexpr uint8_t ledBrightness = 100;
+
+// ! The order of these MUST match the order in PatternType!
+static Pattern patterns[patternCount] = {
+    {.type = PatternType::None,
+     .numStates = 0,
+     .changeDelay = 0,
+     .cb = executePatternNone},
+    {.type = PatternType::SetAll,
+     .numStates = 1,
+     .changeDelay = 500u,
+     .cb = executePatternSetAll},
+    {.type = PatternType::Blink,
+     .numStates = 2,
+     .changeDelay = 100u,
+     .cb = executePatternBlink},
+    {.type = PatternType::RGBFade,
+     .numStates = 256,
+     .changeDelay = 10u,
+     .cb = executePatternRGBFade},
+    {.type = PatternType::HackerMode,
+     .numStates = 2,
+     .changeDelay = 100u,
+     .cb = executePatternHackerMode},
+    {.type = PatternType::Chase,
+     .numStates = ledNum + chaseLEDWidth - 1,
+     .changeDelay = 500u,
+     .cb = executePatternChase},
+    {.type = PatternType::Wipe,
+     .numStates = ledNum,
+     .changeDelay = 500u,
+     .cb = executePatternWipe}};
+
+static volatile uint8_t receiveBuf[receiveBufSize];
+static volatile bool newData = false;
+static Command command;
+static CRGB currentColor;
+static CRGB leds[ledNum];
+static PatternRunner patternRunner(&FastLED, patterns);
+static bool systemOn = true;
+
+void setup() {
+    Wire.begin(slaveAddress);      // join i2c bus with address #4
+    Wire.onReceive(receiveEvent);  // register event
+    Wire.onRequest(requestEvent);
+    Serial.begin(115200);  // start serial for output
+    FastLED.addLeds<WS2812B, ledDataOutPin, GRB>(leds, ledNum);
+    FastLED.setBrightness(ledBrightness);
+
+    // Initialize all LEDs to black
+    executePatternSetAll(leds, CRGB::Black, 0, ledNum);
+    FastLED.show();
+}
+
+void loop() {
+    // If there's new data, process it
+    if (newData) {
+        uint8_t buf[receiveBufSize];
+        noInterrupts();
+        memcpy(buf, (const void *)receiveBuf, receiveBufSize);
+        parseCommand(buf, receiveBufSize);
+        newData = false;
+        switch (command.commandType) {
+            case CommandType::On:
+                // Go back to running the current color and pattern
+                patternRunner.reset();
+                systemOn = true;
+                break;
+
+            case CommandType::Off:
+                // Set LEDs to black and stop running the pattern
+                executePatternSetAll(leds, CRGB::Black, 0, ledNum);
+                FastLED.show();
+                systemOn = false;
+                break;
+
+            case CommandType::Pattern:
+                // To set everything to a certain color, change color then call
+                // the 'set all' pattern
+                patternRunner.setCurrentPattern(
+                    command.commandData.commandPattern.pattern,
+                    command.commandData.commandPattern.oneShot);
+                break;
+
+            case CommandType::ChangeColor: {
+                auto colors = command.commandData.commandColor;
+                currentColor = CRGB(colors.red, colors.green, colors.blue);
+                patternRunner.setCurrentColor(currentColor);
+            } break;
+
+            case CommandType::ReadPatternDone:
+                break;
+
+            default:
+                break;
+        }
+
+        Serial.print(F("ON="));
+        Serial.println(systemOn);
+
+        interrupts();
+    }
+
+    if (systemOn) {
+        patternRunner.update();
+    }
+}
+
+void receiveEvent(int howMany) {
+    Wire.readBytes((uint8_t *)receiveBuf, howMany);
+    newData = true;
+}
+
+void requestEvent() {
+    switch (command.commandType) {
+        case CommandType::ReadPatternDone:
+            Wire.write(patternRunner.patternDone());
+            break;
+
+        default:
+            // Send back 255 (-1 signed) to indicate bad/no data
+            Wire.write(0xff);
+    }
+}
+
+void parseCommand(uint8_t *buf, size_t len) {
+    auto type = (CommandType)buf[0];
+    Serial.print("Received command type=");
+    Serial.println(buf[0]);
+    command.commandType = type;
+    switch (type) {
+        case CommandType::On:
+            command.commandData.commandOn = {};
+            break;
+
+        case CommandType::Off:
+            command.commandData.commandOff = {};
+            break;
+
+        case CommandType::Pattern:
+            memcpy(&command.commandData.commandPattern.pattern, &buf[1], 2);
+            break;
+
+        case CommandType::ChangeColor:
+            memcpy(&command.commandData.commandColor.red, &buf[1], 3);
+            break;
+
+        case CommandType::ReadPatternDone:
+            command.commandData.commandReadPatternDone = {};
+            break;
+
+        default:
+            break;
+    }
+}
+
+bool executePatternNone(CRGB *leds, CRGB color, uint16_t state,
+                        uint16_t ledCount) {
+    return false;
+}
+
+bool executePatternSetAll(CRGB *leds, CRGB color, uint16_t state,
+                          uint16_t ledCount) {
+    for (size_t i = 0; i < ledCount; i++) {
+        leds[i] = color;
+    }
+
+    return true;
+}
+
+bool executePatternBlink(CRGB *leds, CRGB color, uint16_t state,
+                         uint16_t ledCount) {
+    switch (state) {
+        case 0:
+            for (size_t i = 0; i < ledCount; i++) {
+                leds[i] = color;
+            }
+            return true;
+
+        case 1:
+            for (size_t i = 0; i < ledCount; i++) {
+                leds[i] = CRGB::Black;
+            }
+            return true;
+
+        default:
+            return false;
+    }
+}
+
+bool executePatternRGBFade(CRGB *leds, CRGB color, uint16_t state,
+                           uint16_t ledCount) {
+    for (size_t i = 0; i < ledCount; i++) {
+        leds[i] = PatternRunner::Wheel(((i * 256 / ledCount) + state) & 255);
+    }
+    return true;
+}
+
+bool executePatternHackerMode(CRGB *leds, CRGB color, uint16_t state,
+                              uint16_t ledCount) {
+    switch (state) {
+        case 0:
+            return executePatternSetAll(leds, CRGB::Green, 0, ledCount);
+
+        case 1:
+            return executePatternSetAll(leds, CRGB::DarkGreen, 0, ledCount);
+
+        default:
+            return false;
+    }
+}
+
+bool executePatternChase(CRGB *leds, CRGB color, uint16_t state,
+                         uint16_t ledCount) {
+    if (state - 1 >= 0) {
+        leds[state - 1] = CRGB::Black;
+    }
+
+    for (size_t i = state; i < state + chaseLEDWidth; i++) {
+        if (i < ledCount) {
+            leds[i] = color;
+        }
+    }
+    return true;
+}
+
+bool executePatternWipe(CRGB *leds, CRGB color, uint16_t state,
+                        uint16_t ledCount) {
+    leds[state] = color;
+    return true;
+}
---
+++ ledcontroller/testclient/src/main.cpp
@@ -0,0 +1,352 @@
+#include <Arduino.h>
+#include <FastLED.h>
+#include <Wire.h>
+
+constexpr uint8_t slaveAddr = 0x01;
+constexpr uint16_t commandDelayMs = 200;
+
+enum class InputOptions {
+    RunTests = 0,
+    SetOn = 1,
+    SetOff = 2,
+    SetColor = 3,
+    SetPattern = 4,
+    ReadPatternDone = 5,
+    Invalid = 6
+};
+
+enum class CommandType {
+    CommandOn = 0,
+    CommandOff = 1,
+    CommandPattern = 2,
+    CommandColor = 3,
+    CommandReadPatternDone = 4
+};
+
+enum class PatternType {
+    None = 0,
+    SetAll = 1,
+    Blink = 2,
+    RGBFade = 3,
+    HackerMode = 4,
+    Chase = 5,
+    Wipe = 6
+};
+
+void writeCommand(CommandType type, uint8_t *data);
+void setPattern(PatternType pattern, uint8_t oneShot);
+void setColor(CRGB color);
+void setOnOff(bool isOn);
+
+void printMenu();
+InputOptions parseInputOption(char);
+void handleInput(InputOptions);
+void handleRunMainTests();
+void handleOnOff(bool val);
+void handleColor();
+void handlePattern();
+void handleReadPatternDone();
+
+void setup() {
+    Serial.begin(115200);
+    Wire.begin();
+    setOnOff(false);
+    Serial.println(F("Ready. Set your serial monitor to send Newline"));
+}
+
+void loop() {
+    printMenu();
+    while (!Serial.available())
+        ;
+    char input = Serial.read();
+    InputOptions option = parseInputOption(input);
+    if (option == InputOptions::Invalid)
+        ;
+    else {
+        handleInput(option);
+    }
+    Serial.println();
+    while (Serial.available())
+        ;
+    Serial.read();
+}
+
+void runMainTests() {
+    // What happens if we set the color while it's off?
+    Serial.println(F("Setting color to blue"));
+    setColor(CRGB::Blue);
+    Serial.println(F("Setting to on"));
+    setOnOff(true);
+    delay(1000);
+    setPattern(PatternType::SetAll, false);
+    delay(5000);
+    Serial.println(F("Setting color to purple"));
+    setColor(CRGB::Purple);
+    delay(5000);
+    Serial.println(F("Setting color to yellow"));
+    setColor(CRGB::Yellow);
+    delay(5000);
+    Serial.println(F("Setting to off"));
+    setOnOff(false);
+    // Should maintain yellow color
+    Serial.println(F("Setting to on"));
+    setOnOff(true);
+    setColor(CRGB::ForestGreen);
+    Serial.println(F("Setting pattern to set all"));
+    setPattern(PatternType::SetAll, true);
+    delay(1000);
+    Serial.println(F("Setting pattern to blink"));
+    setPattern(PatternType::Blink, false);
+    delay(3000);
+    Serial.println(F("Setting color to blue"));
+    setColor(CRGB::Blue);
+    Serial.println(F("Setting pattern to chase"));
+    setPattern(PatternType::Chase, false);
+    delay(3000);
+    Serial.println(F("Setting color to cyan"));
+    setColor(CRGB::Cyan);
+    delay(1000);
+    Serial.println(F("Setting pattern to hacker mode"));
+    setPattern(PatternType::HackerMode, false);
+    delay(4000);
+    Serial.println(F("Setting pattern to RGB fade"));
+    setPattern(PatternType::RGBFade, false);
+    delay(5000);
+    Serial.println(F("Setting color to magenta"));
+    setColor(CRGB::Magenta);
+    Serial.println(F("Setting pattern to one-shot wipe"));
+    setPattern(PatternType::SetAll, true);
+    delay(4000);
+    setOnOff(false);
+    delay(1000);
+    Serial.println(F("Main tests done"));
+}
+
+void printMenu() {
+    Serial.println(
+        F("Pick ONE option from the available commands. Send as single "
+          "character:"));
+    Serial.println(F("\t't' - Run test sequence"));
+    Serial.println(F("\t'c' - Set the color"));
+    Serial.println(F("\t'Y' - Set to on"));
+    Serial.println(F("\t'N' - Set to off"));
+    Serial.println(F("\t'p' - Set to a pattern"));
+    Serial.println(F("\t'd' - Read if pattern is done running"));
+}
+
+InputOptions parseInputOption(char input) {
+    switch (input) {
+        case 't':
+            return InputOptions::RunTests;
+
+        case 'c':
+            return InputOptions::SetColor;
+
+        case 'Y':
+            return InputOptions::SetOn;
+
+        case 'N':
+            return InputOptions::SetOff;
+
+        case 'p':
+            return InputOptions::SetPattern;
+
+        case 'd':
+            return InputOptions::ReadPatternDone;
+    }
+
+    Serial.print(F("Invalid option "));
+    Serial.println(input);
+    return InputOptions::Invalid;
+}
+
+void handleInput(InputOptions option) {
+    uint8_t optionInt = (uint8_t)option;
+
+    switch (option) {
+        case InputOptions::RunTests:
+            handleRunMainTests();
+            break;
+
+        case InputOptions::SetOn:
+        case InputOptions::SetOff:
+            handleOnOff(optionInt - 1);
+            break;
+
+        case InputOptions::SetColor:
+            handleColor();
+            break;
+
+        case InputOptions::SetPattern:
+            handlePattern();
+            break;
+
+        case InputOptions::ReadPatternDone:
+            handleReadPatternDone();
+            break;
+
+        case InputOptions::Invalid:
+        default:
+            Serial.println(F("Invalid option"));
+    }
+}
+
+void handleRunMainTests() {
+    Serial.println(F("Running main tests..."));
+    runMainTests();
+}
+
+// Val == 0 means ON, 1 is OFF
+void handleOnOff(bool val) {
+    if (!val) {
+        Serial.println(F("Setting to ON"));
+    } else {
+        Serial.println(F("Setting to OFF"));
+    }
+
+    setOnOff(!val);
+}
+
+void handleColor() {
+    Serial.println(
+        F("Enter a color in the form of: R,G,B; where values are in decimal "
+          "format. Don't forget the semicolon at the end!"));
+    Serial.println(F("Example: 50,100,255;"));
+    while (!Serial.available())
+        ;
+    uint8_t r = Serial.readStringUntil(',').toInt();
+    uint8_t g = Serial.readStringUntil(',').toInt();
+    uint8_t b = Serial.readStringUntil(';').toInt();
+    Serial.print(F("Writing color: "));
+    Serial.print(r);
+    Serial.print(',');
+    Serial.print('g');
+    Serial.print(',');
+    Serial.println(b);
+
+    uint8_t buf[3] = {r, g, b};
+    writeCommand(CommandType::CommandColor, buf);
+}
+
+void handlePattern() {
+    Serial.println(F("Enter the desired pattern:"));
+    Serial.println(F("\t'n' - None"));
+    Serial.println(F("\t'a' - Set all"));
+    Serial.println(F("\t'b' - Blink"));
+    Serial.println(F("\t'f' - RGB fade"));
+    Serial.println(F("\t'h' - Hacker mode"));
+    Serial.println(F("\t'c' - Chase"));
+    Serial.println(F("\t'w' - Wipe"));
+
+    while (!Serial.available())
+        ;
+
+    PatternType pattern = PatternType::None;
+    char option = Serial.read();
+    Serial.println(option);
+    switch (option) {
+        case 'a':
+            pattern = PatternType::SetAll;
+            break;
+
+        case 'b':
+            pattern = PatternType::Blink;
+            break;
+
+        case 'f':
+            pattern = PatternType::RGBFade;
+            break;
+
+        case 'h':
+            pattern = PatternType::HackerMode;
+            break;
+
+        case 'c':
+            pattern = PatternType::Chase;
+            break;
+
+        case 'w':
+            pattern = PatternType::Wipe;
+            break;
+
+        case 'n':
+        default:
+            pattern = PatternType::None;
+    }
+
+    Serial.println((uint8_t)pattern);
+    Serial.print(F("Run once (one-shot)? (Y/N): "));
+    while (!Serial.available())
+        ;
+    option = Serial.read();
+    uint8_t oneShot = option == 'Y' ? 1 : 0;
+    setPattern(pattern, oneShot);
+}
+
+void handleReadPatternDone() {
+    Serial.println(F("Reading if pattern is done"));
+    Wire.beginTransmission(slaveAddr);
+    Wire.write((uint8_t)CommandType::CommandReadPatternDone);
+    Wire.endTransmission();
+    Wire.requestFrom(slaveAddr, 1u);
+    uint8_t done = Wire.read();
+    Serial.print("Device returned ");
+    Serial.println(done);
+}
+
+void setPattern(PatternType pattern, uint8_t oneShot) {
+    uint8_t buf[2] = {(uint8_t)pattern, oneShot};
+    writeCommand(CommandType::CommandPattern, buf);
+}
+
+void setColor(CRGB color) {
+    uint8_t buf[] = {color.r, color.g, color.b};
+    writeCommand(CommandType::CommandColor, buf);
+}
+
+void setOnOff(bool isOn) {
+    if (isOn) {
+        writeCommand(CommandType::CommandOn, nullptr);
+        return;
+    }
+
+    writeCommand(CommandType::CommandOff, nullptr);
+}
+
+void writeCommand(CommandType type, uint8_t *data) {
+    Wire.beginTransmission(slaveAddr);
+    Wire.write((uint8_t)type);
+    Serial.print("sending command: ");
+    Serial.print((uint8_t)type);
+
+    for (size_t i = 0; i < 4; i++) {
+        Serial.print(data[i], 16);
+        Serial.print(' ');
+    }
+
+    Serial.println();
+
+    switch (type) {
+        case CommandType::CommandOn:
+        case CommandType::CommandOff:
+            break;
+
+        case CommandType::CommandPattern:
+            Wire.write(data[0]);  // pattern
+            Wire.write(data[1]);  // one-shot
+            break;
+
+        case CommandType::CommandColor:
+            for (uint8_t i = 0; i < 3; i++) {
+                Wire.write(data[i]);  // RGB color bytes
+            }
+            break;
+
+        default:
+            break;
+    }
+
+    Wire.endTransmission();
+
+    delay(commandDelayMs);
+}
