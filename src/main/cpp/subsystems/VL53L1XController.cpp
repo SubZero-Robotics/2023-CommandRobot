d5fc14aa161acac26abc9a012747d6b277484c03
Lint Action <lint-action@samuelmeuli.com>
2023-02-23 20:26:35 +0000

    Fix code style issues with clang_format

--- src/main/cpp/subsystems/VL53L1XController.cpp
+++ src/main/cpp/subsystems/VL53L1XController.cpp
@@ -1,8 +1,6 @@
 #include "subsystems/VL53L1XController.h"
 
-VL53L1XController::VL53L1XController(uint8_t inputPin) :
-    _input(std::make_unique<frc::DigitalInput>(inputPin)) {}
+VL53L1XController::VL53L1XController(uint8_t inputPin)
+    : _input(std::make_unique<frc::DigitalInput>(inputPin)) {}
 
-bool VL53L1XController::getDistanceValid() {
-    return _input->Get();
-}
+bool VL53L1XController::getDistanceValid() { return _input->Get(); }
--- src/main/include/subsystems/VL53L1XController.h
+++ src/main/include/subsystems/VL53L1XController.h
@@ -2,20 +2,21 @@
 #define VL53L1X_CONTROLLER_H
 
 #include <frc/DigitalInput.h>
+
 #include <memory>
 
 class VL53L1XController {
-    public:
+   public:
     VL53L1XController(uint8_t inputPin);
 
     /**
      * @brief Get if lidar distance is in valid range
-     * 
+     *
      * @return bool true if valid
      */
     bool getDistanceValid();
 
-    private:
+   private:
     std::unique_ptr<frc::DigitalInput> _input;
 };
 
