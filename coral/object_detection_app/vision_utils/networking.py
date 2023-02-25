9c15e3d69fba241282490a577e052801073f76ac
having11 <gatoninja236@gmail.com>
2023-02-24 20:44:48 -0600

    fix NT

--- coral/object_detection_app/vision_utils/networking.py
+++ coral/object_detection_app/vision_utils/networking.py
@@ -12,15 +12,14 @@ class Networking:
         self.table = NetworkTables.getTable(table)
         self.streamTable = NetworkTables.getTable('CameraPublisher')
         # TODO: Pass last IP digits as arg from CLI
-        url = f'mjpg:http://10.{teamNum}.24:{port}/stream'
+        url = f'mjpg:http://10.{teamNum}.4:{port}/stream'
         ntPath = '/coral'
         print(f'Placing NT stream URL {url} under {ntPath}')
         self.streamTable.putStringArray(ntPath + '/streams', [url])
         self.streamTable.putString(ntPath + '/description', 'coral')
         self.streamTable.putString(ntPath + '/mode', '320x240 MJPEG 22fps')
         self.streamTable.putStringArray(ntPath + '/modes', ['320x240 MJPEG 22fps'])
-        self.streamTable.putString(ntPath + '/source', f'ip:http://10.{teamNum}.24:{port}/stream')
-        self.streamTable.putString(ntPath + '/', '')
+        self.streamTable.putString(ntPath + '/source', f'ip:http://10.{teamNum}.4:{port}/stream')
 
     def write(self, outputs: List[Output]) -> List[float]:
         outputNumArray: List[float] = [len(outputs)]
