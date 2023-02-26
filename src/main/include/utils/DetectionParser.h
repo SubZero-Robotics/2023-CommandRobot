a499c55052d8f4d79dca7c7a2772c18947a082d3
Lint Action <lint-action@samuelmeuli.com>
2023-02-26 23:24:52 +0000

    Fix code style issues with clang_format

--- src/main/include/utils/DetectionParser.h
+++ src/main/include/utils/DetectionParser.h
@@ -1,14 +1,10 @@
 #ifndef DETECTION_PARSER_H
 #define DETECTION_PARSER_H
-#include <vector>
-#include <utility>
 #include <array>
+#include <utility>
+#include <vector>
 
-enum class ObjectClasses {
-    Cone = 0, 
-    Cube = 1,
-    Merge = 2
-};
+enum class ObjectClasses { Cone = 0, Cube = 1, Merge = 2 };
 
 struct BoundingBox {
     // An ordered pair representing X and Y of the top left coordinate
@@ -20,44 +16,38 @@ struct BoundingBox {
 
     // [topLeftY, topLeftX, bottomRightY, bottomRightX]
     static BoundingBox parse(std::array<float, 4> boxes) {
-        return {
-            .topLeft = std::make_pair(boxes.at(1), boxes.at(0)),
-            .bottomRight = std::make_pair(boxes.at(3), boxes.at(2)),
-            .width = boxes.at(3) - boxes.at(1),
-            .height = boxes.at(2) - boxes.at(0)
-        };
+        return {.topLeft = std::make_pair(boxes.at(1), boxes.at(0)),
+                .bottomRight = std::make_pair(boxes.at(3), boxes.at(2)),
+                .width = boxes.at(3) - boxes.at(1),
+                .height = boxes.at(2) - boxes.at(0)};
     }
 };
 
-struct DetectedObject
-{
+struct DetectedObject {
     ObjectClasses classId;
     float confidence;
     BoundingBox bbox;
 
-    static std::vector<DetectedObject> parse(std::vector<float> flattenedOutputList) {
-
+    static std::vector<DetectedObject> parse(
+        std::vector<float> flattenedOutputList) {
         auto detectedObjects = std::vector<DetectedObject>(10);
 
         int entryNum = flattenedOutputList.at(0);
 
         for (int i = 0; i < entryNum; i++) {
             int startNum = i * 6 + 1;
-            auto bound = std::array<float, 4> ({
-                flattenedOutputList.at(startNum + 2),
-                flattenedOutputList.at(startNum + 3),
-                flattenedOutputList.at(startNum + 4),
-                flattenedOutputList.at(startNum + 5)
-            });
+            auto bound =
+                std::array<float, 4>({flattenedOutputList.at(startNum + 2),
+                                      flattenedOutputList.at(startNum + 3),
+                                      flattenedOutputList.at(startNum + 4),
+                                      flattenedOutputList.at(startNum + 5)});
             DetectedObject box = {
-                .classId = (ObjectClasses) flattenedOutputList.at(startNum),
+                .classId = (ObjectClasses)flattenedOutputList.at(startNum),
                 .confidence = flattenedOutputList.at(startNum + 1),
-                .bbox = BoundingBox::parse(bound)
-            };
+                .bbox = BoundingBox::parse(bound)};
             detectedObjects.push_back(box);
         }
     }
 };
-
 
 #endif
