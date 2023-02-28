23389aac239656d0c9ad32fe6df8fceead10e298
5690 <5690Programmers@gmail.com>
2023-02-27 18:26:39 -0600

    Created base for Limelight system

--- src/main/include/subsystems/AssistSubsystem.h
+++ src/main/include/subsystems/AssistSubsystem.h
@@ -4,15 +4,31 @@
 
 #include <frc2/command/CommandPtr.h>
 #include <frc2/command/SubsystemBase.h>
+#include <frc/geometry/Pose3d.h>
+#include "utils/DetectionParser.h"
+#include <vector>
+#include <networktables/NetworkTableInstance.h>
+#include <cmath>
 
-class ExampleSubsystem : public frc2::SubsystemBase {
+class AssistSubsystem : public frc2::SubsystemBase {
    public:
-    ExampleSubsystem();
+
+    struct ArmPose {
+        double rotationDegree;
+        double armLengthIn;
+    };
+
+    AssistSubsystem();
 
     /**
      * Will be called periodically whenever the CommandScheduler runs.
      */
     void Periodic() override;
+
+
+    frc::Pose3d GetPosition();
+
+    std::vector<DetectionParser::DetectedObject> GetObjects();
 
     /**
      * Will be called periodically whenever the CommandScheduler runs during
@@ -20,7 +36,14 @@ class ExampleSubsystem : public frc2::SubsystemBase {
      */
     void SimulationPeriodic() override;
 
-   private:
+    static ArmPose GetArmPoseFromDistance(double distanceIn, double poleHeightIn) {
+        return {
+            atan(poleHeightIn / distanceIn),
+            hypot(distanceIn, poleHeightIn)
+        };
+    }
+
+    private:
     // Components (e.g. motor controllers and sensors) should generally be
     // declared private and exposed only through public methods.
 };
