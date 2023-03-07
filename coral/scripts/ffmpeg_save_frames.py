9d73446b18e4c98eb8bb1fe3dee7d1b743ede7c0
Evan Rust <having11@users.noreply.github.com>
2023-03-07 14:03:19 -0600

    ffmpeg frame capture

---
+++ coral/scripts/ffmpeg_save_frames.py
@@ -0,0 +1,10 @@
+# pip install python-ffmpeg
+import ffmpeg
+inputStreamUrl = f'http://10.56.90.4:8080/stream'
+(
+    ffmpeg
+    .input(inputStreamUrl)
+    .filter('fps', fps=10, round='up')
+    .output('%s-%%04d.jpg'%(inputStreamUrl[:-4]), **{'qscale:v': 3})
+    .run()
+)
