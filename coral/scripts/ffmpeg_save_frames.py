95b44bed517da6e59de1c8b598e3f2f82be8dd88
Lint Action <lint-action@samuelmeuli.com>
2023-03-08 03:10:36 +0000

    Fix code style issues with Autopep8

--- coral/scripts/ffmpeg_save_frames.py
+++ coral/scripts/ffmpeg_save_frames.py
@@ -5,6 +5,6 @@ inputStreamUrl = f'http://10.56.90.4:8080/stream'
     ffmpeg
     .input(inputStreamUrl)
     .filter('fps', fps=10, round='up')
-    .output('%s-%%04d.jpg'%(inputStreamUrl[:-4]), **{'qscale:v': 3})
+    .output('%s-%%04d.jpg' % (inputStreamUrl[:-4]), **{'qscale:v': 3})
     .run()
 )
