43a57d10b5c2c1081953b7943dc0d2b94ad13fd2
Lint Action <lint-action@samuelmeuli.com>
2023-03-18 01:14:48 +0000

    Fix code style issues with clang_format

--- src/main/include/subsystems/BaseSingleAxisSubsystem.h
+++ src/main/include/subsystems/BaseSingleAxisSubsystem.h
@@ -149,7 +149,8 @@ class BaseSingleAxisSubsystem : public ISingleAxisSubsystem,
                     _prefix, "NOT MOVING; AT HOME" + std::to_string(speed),
                     Logging::Level::VERBOSE);
 
-            _motor.Set(ArmConstants::kAntiGravityPercentage * _config.motorMultiplier);
+            _motor.Set(ArmConstants::kAntiGravityPercentage *
+                       _config.motorMultiplier);
             return;
         }
 
@@ -170,7 +171,8 @@ class BaseSingleAxisSubsystem : public ISingleAxisSubsystem,
                 Logging::logToStdOut(
                     _prefix, "NOT MOVING; AT MAX" + std::to_string(speed),
                     Logging::Level::VERBOSE);
-            _motor.Set(ArmConstants::kAntiGravityPercentage * _config.motorMultiplier);
+            _motor.Set(ArmConstants::kAntiGravityPercentage *
+                       _config.motorMultiplier);
             return;
         }
 
