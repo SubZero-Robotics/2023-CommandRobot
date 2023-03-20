6441f2a177ceaca31b2ee0994e059a1e56c79f31
Evan Rust <having11@users.noreply.github.com>
2023-03-19 21:54:01 -0500

    update stdout logging

--- src/main/include/utils/Logging.h
+++ src/main/include/utils/Logging.h
@@ -12,51 +12,31 @@ enum class Level { VERBOSE, INFO, WARNING, ERROR };
 static std::string getLevelString(const std::string& msg, Level level);
 
 static void logToSmartDashboard(const std::string& key, const std::string& msg,
-                                Level level) {
+                                Level level = Level::INFO) {
     frc::SmartDashboard::PutString(keyPrefix + "-" + key, msg);
 }
 
 static void logToStdOut(const std::string& key, const std::string& msg,
                         Level level) {
-    std::cout << key << " - " << getLevelString(msg, level);
-}
-
-static std::string getLevelString(const std::string& msg, Level level) {
     switch (level) {
         case Level::VERBOSE:
-            return "VERBOSE: " + msg + "\n";
-        case Level::INFO:
-            return "INFO: " + msg + "\n";
-        case Level::WARNING:
-            return "WARNING: " + msg + "\n";
-        case Level::ERROR:
-            return "ERROR: " + msg + "\n";
-        default:
-            return "LOG: " + msg + "\n";
-    }
-}
-
-static void logPretty(const std::string& msg, Level level,
-                      const std::string& key) {
-    switch (level) {
-        case Level::VERBOSE:
-            std::cout << "\033[1;34m"
-                      << "[" << key << "] VERBOSE: " << msg << "\033[0m"
+            std::cout << "\033[34;1m"
+                      << "[" << key << "]\033[34m VERBOSE: " << msg << "\033[0m"
                       << std::endl;
         case Level::INFO:
-            std::cout << "\033[1;32m"
-                      << "[" << key << "] INFO: " << msg << "\033[0m"
+            std::cout << "\033[32;1m"
+                      << "[" << key << "]\033[32m INFO: " << msg << "\033[0m"
                       << std::endl;
         case Level::WARNING:
-            std::cout << "\033[1;33m"
-                      << "[" << key << "] WARNING: " << msg << "\033[0m"
+            std::cout << "\033[33;1m"
+                      << "[" << key << "]\033[33m WARNING: " << msg << "\033[0m"
                       << std::endl;
         case Level::ERROR:
-            std::cout << "\033[1;31m"
-                      << "[" << key << "] ERROR: " << msg << "\033[0m"
+            std::cout << "\033[31;1;7m"
+                      << "[" << key << "]\033[31m ERROR: " << msg << "\033[0m"
                       << std::endl;
         default:
-            std::cout << "\033[1;37m"
+            std::cout << "\033[37m"
                       << "[" << key << "] LOG: " << msg << "\033[0m"
                       << std::endl;
     }
